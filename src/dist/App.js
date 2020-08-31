c73967d4c60ef0311a44a67117e02c3980027e89
Bruno Padilha <brpadilha.dev@gmail.com>
2020-08-31 11:31:32 -0300

    estiliza a página de login

--- src/dist/App.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-var global_1 = require("./styles/global");
-var App = function () { return (react_1["default"].createElement(react_1["default"].Fragment, null,
-    react_1["default"].createElement(global_1["default"], null),
-    react_1["default"].createElement("h1", null, "Hello World"))); };
-exports["default"] = App;
