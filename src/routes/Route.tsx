a0680990cb800035a0e4af2950603af4ff807b59
Bruno Padilha <brpadilha.dev@gmail.com>
2020-09-13 23:26:52 -0300

    faz redirecionamento de rotas caso o usuário esteja logado

---
+++ src/routes/Route.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import {
+  Route as ReactDOMRoute,
+  RouteProps as ReactDOMRouteProps,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+// rota privada / autenticado
+// true/true = ok
+// true/false = Redirecionar para login
+// false/true = Redirecionar para o dashboard
+// false/false = ok
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      // location é historico de rotas
+      // render vai ser para enderizar o componente que estará no route
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
