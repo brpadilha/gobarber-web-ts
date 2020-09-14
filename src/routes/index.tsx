07317766e883fead23c15b2bdb311efc08db5abf
Bruno Padilha <brpadilha.dev@gmail.com>
2020-09-14 11:03:29 -0300

    retira exact das rotas que não são raiz

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,9 +11,9 @@ import Dashboard from '../pages/Dashboard';
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
-    <Route path="/signup" exact component={SignUp} />
+    <Route path="/signup" component={SignUp} />
 
-    <Route path="/dashboard" exact component={Dashboard} isPrivate />
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
