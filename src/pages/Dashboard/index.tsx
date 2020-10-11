719f8c711d8c1ae604c3a0a017420302dc9db8c8
Bruno Padilha <brpadilha.dev@gmail.com>
2020-10-11 14:46:00 -0300

    cria o Header da aplicaçao

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,22 +1,37 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 
-import { Form } from '@unform/web';
-
+import { FiPower } from 'react-icons/fi';
+import { Container, HeaderContent, Header, Profile } from './styles';
+import logo from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
-
-  const handleSubmit = useCallback(() => {
-    signOut();
-  }, [signOut]);
+  const { signOut, user } = useAuth();
 
   return (
     <>
+      <Container>
+        <Header>
+          <HeaderContent>
+            <img src={logo} alt="GoBarber" />
+            <Profile>
+              <img
+                src="https://avatars0.githubusercontent.com/u/37818334?s=460&u=e20caeb3543058fc044ad564275f8777aeb512ad&v=4"
+                alt="profile"
+              />
+              <div>
+                <span>Bem vindo,</span>
+                <strong>{user.name}</strong>
+              </div>
+            </Profile>
+
+            <button type="button" onClick={signOut}>
+              <FiPower size={20} />
+            </button>
+          </HeaderContent>
+        </Header>
+      </Container>
       <h1>Dashboard</h1>
-      <Form onSubmit={handleSubmit}>
-        <button type="submit">LogOut</button>
-      </Form>
     </>
   );
 };
