f38914107c165794edce754b2a975524da4f9a18
Bruno Padilha <brpadilha.dev@gmail.com>
2020-10-12 15:11:00 -0300

    Estiliza agendamento

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, HeaderContent, Header, Profile } from './styles';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  HeaderContent,
+  Header,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 import logo from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
@@ -30,6 +41,76 @@ const Dashboard: React.FC = () => {
             </button>
           </HeaderContent>
         </Header>
+        <Content>
+          <Schedule>
+            <h1>Horários Agendados</h1>
+            <p>
+              <span>Hoje</span>
+              <span>Dia 06</span>
+              <span>Segunda-feira</span>
+            </p>
+            <NextAppointment>
+              <strong>Atendimento a seguir</strong>
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/37818334?s=460&u=e20caeb3543058fc044ad564275f8777aeb512ad&v=4"
+                  alt="cliente"
+                />
+                <strong>Leonardo Minatti</strong>
+                <span>
+                  <FiClock />
+                  08:00
+                </span>
+              </div>
+            </NextAppointment>
+            <Section>
+              <strong>Manhã</strong>
+              <Appointment>
+                <span>
+                  <FiClock />
+                  08:00
+                </span>
+                <div>
+                  <img
+                    src="https://avatars0.githubusercontent.com/u/37818334?s=460&u=e20caeb3543058fc044ad564275f8777aeb512ad&v=4"
+                    alt="cliente"
+                  />
+                  <strong>Leonardo Minatti</strong>
+                </div>
+              </Appointment>
+              <Appointment>
+                <span>
+                  <FiClock />
+                  09:00
+                </span>
+                <div>
+                  <img
+                    src="https://avatars0.githubusercontent.com/u/37818334?s=460&u=e20caeb3543058fc044ad564275f8777aeb512ad&v=4"
+                    alt="cliente"
+                  />
+                  <strong>Leonardo Minatti</strong>
+                </div>
+              </Appointment>
+            </Section>
+            <Section>
+              <strong>Tarde</strong>
+              <Appointment>
+                <span>
+                  <FiClock />
+                  08:00
+                </span>
+                <div>
+                  <img
+                    src="https://avatars0.githubusercontent.com/u/37818334?s=460&u=e20caeb3543058fc044ad564275f8777aeb512ad&v=4"
+                    alt="cliente"
+                  />
+                  <strong>Leonardo Minatti</strong>
+                </div>
+              </Appointment>
+            </Section>
+          </Schedule>
+          <Calendar />
+        </Content>
       </Container>
       <h1>Dashboard</h1>
     </>
